a1cd3185eed7f20c689e5ebf7bde1a4bf2c88ccb
name-of-okja <nameofokja@gmail.com>
2024-07-21 17:33:27 +0900

    Hash Warning

--- 3_Ecommerce/sandbox/index.ts
+++ 3_Ecommerce/sandbox/index.ts
@@ -1,5 +1,20 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	// redis에서 hashSet에 null 이나 undiefined는 대입 할 수 없다.
+	// value가 object 일 경우 object.ToString()이 되므로 원치않는 값이 대입된다.
+	await client.hSet('car', {
+		color: 'red',
+		year: 1950
+	});
+
+	const car = await client.hGetAll('car');
+
+	if (Object.keys(car).length === 0) {
+		console.log('Car Not Exist');
+		return;
+	}
+	console.log(car);
+};
 run();
