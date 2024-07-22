4e681e5cd0134d9db53e638063119d749f41d4a8
name-of-okja <nameofokja@gmail.com>
2024-07-22 20:58:50 +0900

    default hash logic

--- 3_Ecommerce/src/services/keys.ts
+++ 3_Ecommerce/src/services/keys.ts
@@ -1,1 +1,6 @@
 export const pageCacheKey = (id: string) => `pageCache#${id}`;
+
+export const usersKey = (userId: string) => `users#${userId}`;
+export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
+
+export const itemsKey = (itemId: string) => `items#${itemId}`;
