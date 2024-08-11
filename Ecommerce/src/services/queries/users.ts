db0f5ddb3c7c6d0a974afd1bfde007a3fe02ac62
name-of-okja <nameofokja@gmail.com>
2024-08-11 17:04:21 +0900

    impl Default ZSet

--- Ecommerce/src/services/queries/users.ts
+++ Ecommerce/src/services/queries/users.ts
@@ -1,9 +1,21 @@
-import { usernameUniqueKey, usersKey } from '$services/keys';
+import { usernamesKey, usernameUniqueKey, usersKey } from '$services/keys';
 import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+	const decimalId = await client.zScore(usernamesKey(), username);
+
+	if (!decimalId) {
+		throw new Error('User not exist');
+	}
+
+	const id = decimalId.toString(16);
+
+	const user = await client.hGetAll(usersKey(id));
+
+	return deserialize(id, user);
+};
 
 export const getUserById = async (id: string) => {
 	const user = await client.hGetAll(usersKey(id));
@@ -22,6 +34,10 @@ export const createUser = async (attrs: CreateUserAttrs) => {
 
 	await client.hSet(usersKey(id), serialize(attrs));
 	await client.sAdd(usernameUniqueKey(), attrs.username);
+	await client.zAdd(usernamesKey(), {
+		value: attrs.username,
+		score: parseInt(id, 16)
+	});
 
 	return id;
 };
